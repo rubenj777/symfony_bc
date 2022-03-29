e2d814b4e2e98cecf89207528486568b511a2a0c
ruben jallifier-talmat <ruben.jallifier@gmail.com>
2022-03-29 16:18:48 +0200

    added image with Vich uploader bundle

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -8,6 +8,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ProductType extends AbstractType
 {
@@ -19,6 +20,7 @@ class ProductType extends AbstractType
             ->add('isActive')
             ->add('description')
             ->add('category', EntityType::class, ['class'=>Category::class, 'choice_label'=>'name'])
+            ->add('imageFile', VichImageType::class)
         ;
     }
 
